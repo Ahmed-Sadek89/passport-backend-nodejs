25dc5d2e2ae65afc06c924eabcaebeb52efcbe67
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-14 21:47:56 +0200

    commit

--- App.js
+++ App.js
@@ -17,15 +17,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(
-  cors()
+  cors({
+    origin: process.env.CLIENT_ORIGIN,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
 );
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_ORIGIN,
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   })
-// );
 app.use("/auth", authRoute);
 
 
