7e5817b8c8c9c5f1f99c85c908b7c8efc83e1624
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-14 19:56:11 +0200

    complete oauth

--- App.js
+++ App.js
@@ -24,6 +24,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "*")
+  res.setHeader("Access-Control-Allow-Headers", "Authorization")
+  next()
+})
+
 app.use("/auth", authRoute);
 
 
