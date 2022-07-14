6f5878806670b7965eb9f9c0033bb5bd8b6825c9
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-14 20:09:18 +0200

    m

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ app.use(
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_ORIGIN);
-  res.setHeader("Access-Control-Allow-Method", "GET,POST,PUT,DELETE")
+  res.setHeader("Access-Control-Allow-Method", "*")
   res.setHeader("Access-Control-Allow-Headers", "Authorization")
   next()
 })
