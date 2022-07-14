e39211f20befd4081c5a50bca44de3b1dc42ca4b
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-14 21:16:04 +0200

    m

--- App.js
+++ App.js
@@ -15,10 +15,7 @@ app.use(cookieSession({
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true")
-  next()
-})
+
 app.use(
   cors({
     origin: process.env.CLIENT_ORIGIN,
