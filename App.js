28ad1956f407ee57a312be45a7aedc511420433e
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-16 22:14:05 +0200

    sadek

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-// const cookieSession = require("cookie-session");
-const session = require('express-session')
-const sessionStore = require('connect-mongodb-session')(session);
+const cookieSession = require("cookie-session");
+// const session = require('express-session')
+// const sessionStore = require('connect-mongodb-session')(session);
 const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose');
@@ -10,23 +10,10 @@ const authRoute = require("./Routes/Auth.route");
 const app = express();
 require('dotenv').config()
 
-// app.use(
-//   cookieSession({ name: "session", keys: ["sadek"], maxAge: 24 * 60 * 60 * 100 })
-// );
-const STORE  = new sessionStore({
-  url: process.env.DB_CONNECT,
-  collection: 'sessions'
-})
-app.use(session({
-  secret: 'sdkfjdhaisdjimckdsjejimcei',
-  saveUninitialized: false,
-  
-  cookie: {
-    maxAge: 24 * 60 * 60 * 100,
-    httpOnly: true
-  },
-  store: STORE
-}))
+app.use(
+  cookieSession({ name: "session", keys: ["sadek"], maxAge: 24 * 60 * 60 * 100 })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
