ef531367a35f0232577e4df34e58be3d61fff29e
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-17 19:10:36 +0200

    sadek

--- App.js
+++ App.js
@@ -9,18 +9,19 @@ require('dotenv').config()
 app.use(
   cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
 );
-app.use(
-  cors({
-    origin: true,
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type, Authorization",
-  })
-);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(
+  cors({
+    origin: "https://passport-fronend-react.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
-app.use("/auth", authRoute);
+app.use("/auth",cors(),  authRoute);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
