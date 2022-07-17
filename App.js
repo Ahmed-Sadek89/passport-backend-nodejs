980975835d02a37c3998005ec37d856e8f6eaf7b
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-17 20:30:55 +0200

    sadek

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const app = express();
 require('dotenv').config()
 app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000,
-  keys: [keys.session.cookieKey]
+  keys: ['keys.session.cookieKey']
 }));
 app.use(passport.initialize());
 app.use(passport.session());
