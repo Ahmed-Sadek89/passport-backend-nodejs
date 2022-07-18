ab17947ec6d8f36b1873021e90bc3f18218479b6
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-18 16:46:11 +0200

    sadek

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 const User = require('../Model/User.model')
 
-const CLIENT_URL = "http://localhost:3000/";
+const CLIENT_URL = "https://passport-fronend-react.vercel.app/";
 
 router.get("/login/success", async (req, res) => {
   const user = await User.find().sort({date: -1}).limit(1)
