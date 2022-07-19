73cd2e87c66fdb850a045cc6a6e0abc29efff6ca
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-07-19 19:24:51 +0200

    final commit

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config()
 
-app.use(express.json())
+// app.use(express.json())
 app.set("trust proxy", 1);
 
 app.use(
@@ -17,7 +17,7 @@ app.use(
     secret: "secretcode",
     resave: true,
     saveUninitialized: true,
-    cookie: {
+    cookie: { // for HTTPS
       sameSite: "none",
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
